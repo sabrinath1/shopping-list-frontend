83f91417515585db87a843e50240c16a2be82dfd
Sabrina <sabrinath1@hotmail.com>
2022-08-02 15:25:17 -0300

    destrou importation module item and list vue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import Item from "./components/Item.vue";
-import List from "./components/List.vue";
 import "normalize.css";
 import "@/assets/css/main.css";
 
