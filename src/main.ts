f1fbe2df0d0dd4affc1fbec4d6c7fef43866dfa0
Sabrina <sabrinath1@hotmail.com>
2022-08-02 17:06:33 -0300

    deleted importation item and list vue

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "normalize.css";
 import "@/assets/css/main.css";
+import "@/assets/css/destroyButton.css";
+import "@/assets/css/formList.css";
+import "@/assets/css/globalButton.css";
+import "@/assets/css/itemList.css";
 
 createApp(App).mount("#app");
